0f6b6debbef7649a76b619bd3fcce6eccd1c1538
陈烁 <>
2023-07-20 13:55:21 +0800

    1.修复视频缓冲中，bufferingSomeSecond方法先暂停，3秒后还在缓冲中，导致的不会继续播放的问题

--- ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.m
+++ ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.m
@@ -307,11 +307,12 @@ static NSString *const kPresentationSize         = @"presentationSize";
     self.isBuffering = YES;
     
     // 需要先暂停一小会之后再播放，否则网络状况不好的时候时间在走，声音播放不出来
-    [self.player pause];
+    [self pause];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         // 如果此时用户已经暂停了，则不再需要开启播放了
         if (!self.isPlaying && self.loadState == ZFPlayerLoadStateStalled) {
             self.isBuffering = NO;
+            if (!self.playerItem.isPlaybackLikelyToKeepUp) [self bufferingSomeSecond];
             return;
         }
         [self play];
